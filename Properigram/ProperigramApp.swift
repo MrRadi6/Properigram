1c45b3643d8fadad96cafec35b80538552b08701
Ahmed Samir <ahmed.samir@espace.com.eg>
2022-06-24 20:36:43 +0200

    Initial Commit

---
+++ Properigram/ProperigramApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProperigramApp.swift
+//  Properigram
+//
+//  Created by Samir on 6/24/22.
+//
+
+import SwiftUI
+
+@main
+struct ProperigramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
